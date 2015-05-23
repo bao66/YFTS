a96e4fe6b324eb95dafcf0e71619e1e57542afee
bao66 <baoy@go.wustl.edu>
2015-05-23 18:20:35 -0400

    CSV

--- src/com/mercury/template/CsvRW.java
+++ src/com/mercury/template/CsvRW.java
@@ -17,76 +17,82 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.AnnotationConfiguration;
 
-
 import com.mercury.beans.Transactions;
 
 public class CsvRW {
-	public static List<Transactions> csvRead(String readPath){
-		try{
-			List<Transactions> list=new ArrayList<Transactions>();
-			FileInputStream fis=new FileInputStream(readPath);
-			InputStreamReader isw=new InputStreamReader(fis);
+	public static List<Transactions> csvRead(String readPath) {
+		try {
+			List<Transactions> list = new ArrayList<Transactions>();
+			FileInputStream fis = new FileInputStream(readPath);
+			InputStreamReader isw = new InputStreamReader(fis);
 			BufferedReader in = new BufferedReader(isw);
-			String inline=in.readLine();
-			while(inline!=null){
-				String[] tokens=inline.split(",");
-				Transactions ts=new Transactions(tokens[0],tokens[1],Integer.parseInt(tokens[2]),
-						Integer.parseInt(tokens[3]),tokens[4],tokens[5],tokens[6],
-						Double.parseDouble(tokens[7]),Double.parseDouble(tokens[8]));
+			String inline = in.readLine();
+			while (inline != null) {
+				String[] tokens = inline.split(",");
+				Transactions ts = new Transactions(tokens[0], tokens[1],
+						Integer.parseInt(tokens[2]),
+						Integer.parseInt(tokens[3]), tokens[4], tokens[5],
+						tokens[6], Double.parseDouble(tokens[7]),
+						Double.parseDouble(tokens[8]));
 				list.add(ts);
-				inline=in.readLine();
-			}	
+				inline = in.readLine();
+			}
 			in.close();
 			isw.close();
 			fis.close();
 			return list;
-		}catch(Exception e){
+		} catch (Exception e) {
 			System.out.println(e);
 		}
 		return null;
 	}
-	
-	public static void csvWrite(Transactions ts, String writePath){
-		try{
-			FileWriter fw=new FileWriter(writePath,true);
-			String s=new String(ts.getUsername()+","+ts.getCompanyName()+","+String.valueOf(ts.getBuyOrSell())+","+
-					String.valueOf(ts.getQuantity())+","+ts.getTransTime()+","+ts.getStock_companyName()+","+
-					ts.getTrader_username()+","+String.valueOf(ts.getUnitPrice())+","+String.valueOf(ts.getTotalPrice()));
-			fw.write(s+"\n");
+
+	public static void csvWrite(Transactions ts, String writePath) {
+		try {
+			FileWriter fw = new FileWriter(writePath, true);
+			String s = new String(ts.getUsername() + "," + ts.getCompanyName()
+					+ "," + String.valueOf(ts.getBuyOrSell()) + ","
+					+ String.valueOf(ts.getQuantity()) + ","
+					+ ts.getTransTime() + "," + ts.getStock_companyName() + ","
+					+ ts.getTrader_username() + ","
+					+ String.valueOf(ts.getUnitPrice()) + ","
+					+ String.valueOf(ts.getTotalPrice()));
+			fw.write(s + "\n");
 			fw.close();
-		}catch(IOException e){
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
 	}
-	
-	public static void deleteCsv(String deletePath){
-		try{
-			new FileOutputStream(deletePath,false);
-		}catch(IOException e){
+
+	public static void deleteCsv(String deletePath) {
+		try {
+			new FileOutputStream(deletePath, false);
+		} catch (IOException e) {
 			System.out.println(e);
 		}
 	}
-	
-	//The main function is only for testing
+
+	// The main function is only for testing
 	@SuppressWarnings("unchecked")
-	public static void main(String[] args){
-		SessionFactory factory = new AnnotationConfiguration().configure().buildSessionFactory();
-		Session session=factory.openSession();
-		
-		String hql="from Transactions";
-		Query query=session.createQuery(hql);
+	public static void main(String[] args) {
+		SessionFactory factory = new AnnotationConfiguration().configure()
+				.buildSessionFactory();
+		Session session = factory.openSession();
+
+		String hql = "from Transactions";
+		Query query = session.createQuery(hql);
 		System.out.println("*********************");
-		List<Transactions> list=query.list();
-		String path="test.csv";
+		List<Transactions> list = query.list();
+		String path = "test.csv";
 		deleteCsv(path);
-		for (Transactions t:list){
-			csvWrite(t,path);
+		for (Transactions t : list) {
+			csvWrite(t, path);
 		}
-		
-		List<Transactions> list2=new ArrayList<Transactions>();
-		list2=csvRead(path);
-		for(Transactions t2:list2){
+
+		List<Transactions> list2 = new ArrayList<Transactions>();
+		list2 = csvRead(path);
+		for (Transactions t2 : list2) {
 			System.out.println(t2);
 		}
 	}
