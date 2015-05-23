be9ac7394f654875fb27bf73ce69d40ca6160082
bao66 <baoy@go.wustl.edu>
2015-05-23 17:44:27 -0400

    add deletePath in TransactionsDao/deleteCSV

--- src/com/mercury/daos/TransactionsDao.java
+++ src/com/mercury/daos/TransactionsDao.java
@@ -7,7 +7,7 @@ import com.mercury.beans.Transactions;
 public interface TransactionsDao {
 	public void saveToCSV(Transactions transaction);
 	public void saveToDatabase(Transactions transaction);
-	public void deleteCSV();
+	public void deleteCSV(String deletePath);
 	public List<Transactions> queryAllByUsername(String username);
 	public List<Transactions> queryAll();// for admin purpose
 }
--- src/com/mercury/daos/impl/TransactionsDaoImpl.java
+++ src/com/mercury/daos/impl/TransactionsDaoImpl.java
@@ -2,15 +2,24 @@ package com.mercury.daos.impl;
 
 import java.util.List;
 
+import org.hibernate.SessionFactory;
+import org.springframework.orm.hibernate3.HibernateTemplate;
+
 import com.mercury.beans.Transactions;
 import com.mercury.daos.TransactionsDao;
+import com.mercury.template.CsvRW;
 
 public class TransactionsDaoImpl implements TransactionsDao {
+	private HibernateTemplate template;
+
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		template = new HibernateTemplate(sessionFactory);
+	}
 
 	@Override
 	public void saveToCSV(Transactions transaction) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
@@ -20,9 +29,9 @@ public class TransactionsDaoImpl implements TransactionsDao {
 	}
 
 	@Override
-	public void deleteCSV() {
+	public void deleteCSV(String deletePath) {
 		// TODO Auto-generated method stub
-
+		CsvRW.deleteCsv(deletePath);
 	}
 
 	@Override
@@ -31,10 +40,12 @@ public class TransactionsDaoImpl implements TransactionsDao {
 		return null;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<Transactions> queryAll() {
 		// TODO Auto-generated method stub
-		return null;
+		String hql = "from Transactions";
+		return template.find(hql);
 	}
 
 }
