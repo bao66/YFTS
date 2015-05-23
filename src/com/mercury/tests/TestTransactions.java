c51d0764d3507b686ecdd5f592b03d276fb435b5
bao66 <baoy@go.wustl.edu>
2015-05-23 19:14:28 -0400

    Format transaction time

--- src/com/mercury/tests/TestTransactions.java
+++ src/com/mercury/tests/TestTransactions.java
@@ -1,7 +1,10 @@
 package com.mercury.tests;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.List;
+import java.util.TimeZone;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
@@ -12,23 +15,31 @@ import org.hibernate.cfg.AnnotationConfiguration;
 import com.mercury.beans.Transactions;
 
 public class TestTransactions {
+
 	@SuppressWarnings("unchecked")
-	public static void main(String[] args){
-		SessionFactory factory = new AnnotationConfiguration().configure().buildSessionFactory();
-		Session session=factory.openSession(); 
-		Transaction tx=session.beginTransaction();
-		@SuppressWarnings("deprecation")
-		Transactions transactions=new Transactions("Yang","FB",1,50,Calendar.getInstance().getTime().toGMTString(),"FB","Yang",2.31,50*2.31);
+	public static void main(String[] args) {
+		SessionFactory factory = new AnnotationConfiguration().configure()
+				.buildSessionFactory();
+		Session session = factory.openSession();
+		Transaction tx = session.beginTransaction();
+
+		SimpleDateFormat sdf = new SimpleDateFormat();// format time
+		sdf.applyPattern("MM-dd-yyyy HH:mm:ss.S");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+		Transactions transactions = new Transactions("Yang", "FB", 1, 50,sdf.format(Calendar.getInstance().getTime()), "FB", "Yang",2.31, 50 * 2.31);
 		session.save(transactions);
 		tx.commit();
 
-		String hql="from Transactions";
-		Query query=session.createQuery(hql);
+		String hql = "from Transactions";
+		Query query = session.createQuery(hql);
 		System.out.println("*********************");
-		List<Transactions> list=query.list();
-		for (Transactions t:list){
+		List<Transactions> list = query.list();
+		for (Transactions t : list) {
 			System.out.println(t);
 		}
+		System.out.println(sdf.format(Calendar.getInstance().getTime()));
+		session.close();
 	}
 
 }
